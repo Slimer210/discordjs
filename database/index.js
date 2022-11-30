225e35617ac9d6ba24191215de5562777201c717
Slimer210 <howweishan0210@outlook.com>
2022-11-30 11:03:02 +0800

    Update Bot

--- database/index.js
+++ database/index.js
@@ -3,15 +3,58 @@ const fml = require("./fuck");
 const logger = require("npmlog");
 require('dotenv').config();
 
-logger.info("[DB] Connecting Database")
 const DB_ENDPOINT = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.qp86kcn.mongodb.net/?retryWrites=true&w=majority`
 
-mongodb.MongoClient.connect(DB_ENDPOINT);
-logger.info("[DB] Database Connection Success")
+function connect() {
+    logger.info("[DB] Connecting Database")
+    
+
+    // var connection = mongodb.MongoClient.connect(DB_ENDPOINT);
+    // logger.info("[DB] Database Connection Success")
+    // const db = connection.db("guildConfig");
+    // const collection = db.collection("config");
+    // connection.close();
+
+    mongodb.MongoClient.connect(DB_ENDPOINT, function(err, database) {
+        if (err) throw err;
+        logger.info("[DB] Success")
+        const datab = database.db("werewolf");
+        const coll = datab.collection("guildConfig");
+        database.close();
+      });
+}
+
+function insertGuildConfig(guildID) {
+    mongodb.MongoClient.connect(DB_ENDPOINT, function(err, database) {
+        if (err) throw err;
+        logger.info("[DB] Success")
+        const datab = database.db("werewolf");
+        const coll = datab.collection("guildConfig");
+        coll.insertOne({ 
+            guild_id: 'Jackie Robinson', 
+            admin_role: 'admin',
+            player_role: 'player',
+            game_channel: 'game',
+            
+        });
+            
+        database.close();
+      });
+    
+}
 
 function readGuildConfig(guildID) {
     console.log(guildID);
+    mongodb.MongoClient.connect(DB_ENDPOINT, function(err, database) {
+        if (err) throw err;
+        logger.info("[DB] Success")
+        const datab = database.db("werewolf");
+        const coll = datab.collection("guildConfig");
+        database.close();
+    });
 }
 
+module.exports.insertGuildConfig = insertGuildConfig;
 module.exports.readGuildConfig = readGuildConfig;
 module.exports.lut = require("./fuck");
+module.exports.connect = connect;
--- register.js
+++ register.js
@@ -2,18 +2,7 @@ const { REST, Routes } = require('discord.js');
 const cute = require('dotenv').config();
 
 const commands = [
-	{
-		name: 'ping',
-		description: 'Replies with Pong!',
-	},
-    {
-        name: 'kill',
-        description: '(DEV) Kill the server',
-    },
-	{
-		name: 'time',
-		description: 'Displays current time of the server',
-	},
+	
 ];
 
 const rest = new REST({ version: '10' }).setToken(process.env.BOT_TOKEN);
